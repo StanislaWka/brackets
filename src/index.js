a2117be6d613d5c0e0cd9ae7cda88aa823545f13
StanislaWka <stas-babitsky@mail.ru>
2021-02-25 16:01:29 +0300

    fix: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let flg1, flg2, flg3, flg4;    // flg1 - () ...  flg2 - [] ... flg3 - {} ... flg4 - ||
+    str = str.split('');
+    let flg = 0;
+    let mas = [];
+    for (let a of str) {
+        if (a === '(' || a === '[' || a === '{' || a === '|') {
+            if (mas[mas.length - 1] === '|' && a === '|') {
+                mas.pop();
+            } else {
+                mas.push(a);
+            }
+        }
+        if (a === ')' /*|| a === ']' || a === '}' || a === '|'*/) {
+            if (mas[mas.length - 1] === '(') {
+                mas.pop()
+            } else return false;
+        }
+        if (a === ']') {
+            if (mas[mas.length - 1] === '[') {
+                mas.pop()
+            } else return false;
+        }
+        if (a === '}') {
+            if (mas[mas.length - 1] === '{') {
+                mas.pop()
+            } else return false;
+        }
+
+    }
+
+    if (str[str.length -1] == 6 || str[str.length - 1] == 8 || mas.length !== 0) return false;
+
+    for (let a of bracketsConfig) {
+        if (a[0] === '(' && a[1] !== ')') return false;
+        if (a[0] === '{' && a[1] !== '}') return false;
+        if (a[0] === '[' && a[1] !== ']') return false;
+        if (a[0] === '|' && a[1] !== '|') return false;
+    }
+
+    return true;
+
 }
